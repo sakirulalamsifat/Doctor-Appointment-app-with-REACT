dbf5cee0bf5d8d3a465e53c62346dba13688b440
sakirulalamsifat <46572444+sakirulalamsifat@users.noreply.github.com>
2021-04-17 07:09:44 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
         <main className='py-3'>
